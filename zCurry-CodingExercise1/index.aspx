01a8e8bb4ba598f8033891b03ee28006b6da82cc
Zachary Curry <curry.zachary.a@gmail.com>
2019-02-04 20:32:20 -0600

    not my best work, glad to see that I have improved and learned a lot since

---
+++ zCurry-CodingExercise1/index.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="index.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Add" OnClick="Button1_Click" />
+            <br />
+            1. Enter a word into this text box.<br />
+            2. Click "Add" to add this word to the table. <br />
+            <asp:GridView ID="GridView1" runat="server" ShowHeader="False"></asp:GridView>
+            <br />
+            <div id="secondSection" runat="server" Visible="False" >
+                <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                <asp:Button ID="Button2" runat="server" Text="Remove" OnClick="Button2_Click" />
+                <br />
+                1. Enter a word into this text box.<br />
+                2. Click "Remove" to remove this word from the table. <br />        
+            </div>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ zCurry-CodingExercise1/index.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+public partial class _Default : System.Web.UI.Page {
+    List<String> wordList = new List<String>();
+    DataTable dt = new DataTable();
+
+    protected void Page_Load(object sender, EventArgs e) {
+        if (Session["myWordList"] == null) {
+            Session["myWordList"] = wordList;
+        }
+        else
+           secondSection.Visible = true;
+        wordList = (List<String>)Session["myWordList"];
+        dt.Columns.Add(new DataColumn());
+        dt.Columns.Add(new DataColumn());
+        dt.Columns.Add(new DataColumn());
+        dt.Columns.Add(new DataColumn());
+        populateTable();
+    }
+    
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        wordList.Add(TextBox1.Text.ToString());
+        wordList.Sort();
+        Session["myWordList"] = wordList;
+        populateTable();
+    }
+
+    protected void Button2_Click(object sender, EventArgs e) {
+        wordList.Remove(TextBox2.Text.ToString());
+        wordList.Sort();
+        Session["myWordList"] = wordList;
+        populateTable();
+    }
+
+    protected void populateTable() {
+        int wordCount = wordList.Count;
+        int byFour = wordCount / 4;
+        int byFourRemain = wordCount % 4;
+        int rowCount = 0;
+        int wordsRemaining = wordCount;
+        int wordsLastRow;
+
+        dt.Clear();
+
+        for (int i = 0; i < wordCount - 3; i += 4) {
+            int j = i + 1;
+            int k = i + 2;
+            int l = i + 3;
+            dt.Rows.Add(wordList[i], wordList[j], wordList[k], wordList[l]);
+            wordsRemaining -= 4;
+            rowCount++;
+        }
+        wordsLastRow = rowCount * 4;
+        wordsLastRow += wordsRemaining;
+
+        if (byFourRemain > 0 == true) {
+            if (byFourRemain > 1 == true) {
+                if (byFourRemain > 2 == true) {
+                    int i = wordsLastRow - 3;
+                    int j = wordsLastRow - 2;
+                    int k = wordsLastRow - 1;
+                    dt.Rows.Add(wordList[i], wordList[j], wordList[k], null);
+                }
+                else {
+                    int i = wordsLastRow - 2;
+                    int j = wordsLastRow - 1;
+                    dt.Rows.Add(wordList[i], wordList[j], null, null);
+                }
+            }
+            else {
+                int i = wordsLastRow - 1;
+                dt.Rows.Add(wordList[i], null, null, null);
+            }
+        }
+
+        GridView1.DataSource = dt;
+        GridView1.DataBind();
+        secondSection.Visible = true;
+        if (dt.Rows.Count == 0) {
+            secondSection.Visible = false;
+        }
+    }
+}
